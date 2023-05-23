f7755ee2c4d89ef6ddb8eff556996d69faefff4b
Harry Dodson <harry.lee.dodson@gmail.com>
2023-05-23 12:05:53 -0400

    fixed the image positioning along with other elements.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,4 @@
+import './Home.css';
 import ImageContainer from './ImageContainer';
 import ImageLinkForm from './ImageLinkForm';
 import Rank from './Rank';
@@ -6,11 +7,11 @@ import Rank from './Rank';
 export default function home({ faceUrl, setInput, onSubmitImageLink, box }) {
 
     return(
-        <>
+        <div className='home-container'>
             <Rank />
             <p className='description' style={{ 'fontSize': '1.5rem' }}>This magic brain will detect faces in your pictures. Give it a try.</p>
             <ImageLinkForm setInput={setInput} onSubmitImageLink={onSubmitImageLink} />
             <ImageContainer faceUrl={faceUrl} box={box} />
-        </>
+        </div>
     )
 }
--- src/components/home/ImageContainer.jsx
+++ src/components/home/ImageContainer.jsx
@@ -3,9 +3,11 @@ import './ImageContainer.css';
 // prettier-ignore
 export default function imageContainer({ faceUrl, box }) {
   return (
-    <div className='image-container' style={{'position': 'absolute'}}>
-      <img id='inputImage' src={faceUrl} alt='' width='500px' height='auto' />
-      <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+    <div className="image-container">
+      <div style={{'position': 'absolute'}}>
+        <img id='inputImage' src={faceUrl} alt='' width='500px' height='auto' />
+        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      </div>
     </div>
   );
 }

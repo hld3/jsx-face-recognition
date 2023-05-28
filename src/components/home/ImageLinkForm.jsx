808998f0dcbd7f30a0fd38853cb01367b02d66d2
Harry Dodson <harry.lee.dodson@gmail.com>
2023-05-28 16:29:12 +0000

    Updated to increment the user's image count.

--- src/components/home/ImageLinkForm.jsx
+++ src/components/home/ImageLinkForm.jsx
@@ -4,23 +4,48 @@ import { InitialContext } from '../../context/InitialState';
 import './ImageLinkForm.css';
 
 export default function ImageLinkForm() {
-  const { setFaceUrl, input, setInput, setBox } = useContext(InitialContext);
+  const { user, setUser, setFaceUrl, input, setInput, setBox } =
+    useContext(InitialContext);
 
-  async function onSubmitImageLink() {
-    const response = await fetch('http://localhost:3000/image_data', {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ input: input }),
-    });
-    const result = await response.json();
+  async function submitImage() {
+    let result;
+
+    try {
+      const response = await fetch('http://localhost:3000/image_data', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ input: input }),
+      });
+      result = await response.json();
+    } catch (error) {
+      console.log('There was an error retrieving image data.', error);
+    }
+
+    if (result.outputs.length > 0) {
+      await incrementImageCount();
+    }
     setBox(calculateFaceLocation(result));
+  }
+
+  async function incrementImageCount() {
+    try {
+      const incCountResponse = await fetch('http://localhost:3000/image', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: user.id }),
+      });
+      const newCount = await incCountResponse.json();
+      setUser(Object.assign(user, { entries: newCount }));
+    } catch (error) {
+      console.log('There was an error incrementing the user count.', error);
+    }
   }
 
   // prettier-ignore
   return (
         <div className='input-form'>
             <input type='text' className='input' onChange={(e) => setInput(e.target.value)} />
-            <button className='submit-face' onClick={() => {setFaceUrl(input); onSubmitImageLink()}}>Detect</button>
+            <button className='submit-face' onClick={() => {setFaceUrl(input); submitImage()}}>Detect</button>
         </div>
     )
 }

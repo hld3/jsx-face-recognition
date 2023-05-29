7feaffb0f0fcfe6f878eb08d7cb3f7900d3fcade
Harry Dodson <harry.lee.dodson@gmail.com>
2023-05-29 15:54:05 +0000

    Updated to add registered users.

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,19 +1,53 @@
-import '../box.css'
+/* eslint-disable react/prop-types */
+import { useContext, useState } from 'react';
+import { InitialContext } from '../../context/InitialState';
+import '../box.css';
 
-export default function register({ setPage }) {
-    
-    return (
-        <article className='article'>
-            <fieldset id='fieldset'>
-                <legend id='legend-text'>Register</legend>
-                <label htmlFor='name'>Name</label>
-                <input type='text' id='name'/>
-                <label htmlFor='email'>Email</label>
-                <input type='email' id='email'/>
-                <label htmlFor='password'>Password</label>
-                <input type='password' id='password'/>
-            </fieldset>
-            <button onClick={() => setPage('home')}>Sign Up</button>
-        </article>
-    )
+export default function Register({ setPage }) {
+  const { setUser } = useContext(InitialContext);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function registerNewUser() {
+    try {
+      const response = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: name, email: email, password: password }),
+      });
+      const newUser = await response.json();
+      setUser(newUser);
+      setPage('home');
+    } catch (error) {
+      console.log('There was an error during registration.', error);
+    }
+  }
+
+  return (
+    <article className='article'>
+      <fieldset id='fieldset'>
+        <legend id='legend-text'>Register</legend>
+        <label htmlFor='name'>Name</label>
+        <input
+          type='text'
+          id='name'
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          id='email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          id='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </fieldset>
+      <button onClick={registerNewUser}>Sign Up</button>
+    </article>
+  );
 }
